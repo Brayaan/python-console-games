7241efd3fc3666040b79066f5433eb423f2bb98d
Brayaan <menesesvelasco@gmail.com>
2026-05-07 10:03:35 -0500

    v3

--- cliente-LINUX.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Cliente — descubrimiento automático de servidores en la LAN
-gnome-terminal -- bash -c "python3 run_client.py; echo ''; read -p 'Presiona enter para salir...'"
--- cliente-MAC.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Cliente — descubrimiento automático de servidores en la LAN
-python3 run_client.py
-read -p "Presiona enter para salir..."
